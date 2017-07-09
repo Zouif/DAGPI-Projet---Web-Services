32dbc2d429903767c10af63aa439ad983c1d1154
Zouif <draken40230@hotmail.fr>
2017-07-09 23:52:48 +0200

    Modif inscription.php de type SOLIDE

--- site/inscription.php
+++ site/inscription.php
@@ -1,24 +1,65 @@
-<h1>Index</h1>
 <?php
-include('init.php');
+require_once('init.php');
 
-if (!isset($_SESSION['logged'])) {
-			// Login form sent
-				if( isset($_POST['login']) && isset($_POST['passsword']) ) {
-					$query = 'SELECT count(1) AS COUNT FROM company WHERE login = :login';
-					$statement = $pdo->prepare($query, array(PDO::ATTR_CURSOR => PDO::CURSOR_FWDONLY));
-					$statement->execute(array(':login' => $_POST['login']));
-
-					if ($var = $statement->fetch()) {
-						var_dump($var);
-					}
-				}
-		?>
-	<form method="POST">
-		<p>Identifiant : <input type="text" name="login"/></p>
-		<p>Mot de passe : <input type="password" name="password"/></p>
-		<input type="submit" value="Envoyer">
-	</form>
-	<?php
+$currentPage = 'inscription';
+$result = false;
+if( isset($_POST) && !empty($_POST['login']) && !empty($_POST['password']) && !empty($_POST['spreadsheet_id']) ) {
+	$querytest = 'SELECT * FROM company WHERE login = :login';
+	$statementtest = $pdo->prepare($querytest, array(PDO::ATTR_CURSOR => PDO::CURSOR_FWDONLY));
+	$statementtest->execute(array(':login' => $_POST['login']));
+	$resulttest = $statementtest->fetch();
+	if ( empty($resulttest['login']) ){
+		$query = 'INSERT INTO company (login,password,spreadsheet_id) VALUES (:login,:password,:spreadsheet_id)';
+		$statement = $pdo->prepare($query, array(PDO::ATTR_CURSOR => PDO::CURSOR_FWDONLY));
+		$result = $statement->execute(array(
+						':login' => $_POST['login'],
+						':password' => md5($_POST['password']),
+						':spreadsheet_id' => $_POST['spreadsheet_id']
+						));
+	}
 }
+
 ?>
+
+<html>
+<head>
+    <?php include('includes/header.php'); ?>
+</head>
+<body>
+<h1>Inscription</h1>
+<main>
+    <?php
+    if ($result) {
+        ?>
+		
+        <?php
+    }
+    include('includes/menu.php');
+    ?>
+        <?php
+        if ($result) {
+            echo '<h1>Inscription Faite</h1>';
+            ?>
+            <?php
+        }
+        // If logged, display disconnect button
+        if (!isset($_SESSION['logged'])) {
+                ?>
+				<form method="POST">
+					<p>Identifiant : <input type="text" name="login"/></p>
+					<p>Mot de passe : <input type="password" name="password"/></p>
+					<p>Id du Spreadsheet : <input type="text" name="spreadsheet_id"/></p>
+					<input type="submit" value="Envoyer">
+				</form>
+                <?php
+            // If logged, display ?
+        } else {
+            ?>
+
+            <?php
+        }
+        ?>
+    </section>
+</main>
+</body>
+</html>
