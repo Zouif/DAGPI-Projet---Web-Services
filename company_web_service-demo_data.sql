f82c17cb81523c59a80f7b3be18a9587e954daa1
Benjamin BARBE <ben.barbe10@gmail.com>
2017-07-10 13:35:59 +0200

    Updated sql scripts

---
+++ company_web_service-demo_data.sql
@@ -0,0 +1,7 @@
+LOCK TABLES `company` WRITE;
+INSERT INTO `company` VALUES (1,'dagpi','f7ca48b4e4375f4f1df0c374ae4e4b43 ','1_-r1howh0qoDUZluj2OO-JmDWRgcoztLUcmWVu_daQM');
+UNLOCK TABLES;
+
+LOCK TABLES `company_meeting` WRITE;
+INSERT INTO `company_meeting` VALUES (1,1,'Réunion','2017-07-05 16:00:00','2017-07-05 17:00:00','Talence'),(2,1,'Réunion 2','2017-07-06 16:00:00','2017-07-06 18:00:00','Pessac'),(3,1,'Réunion 3','2017-07-07 16:00:00','2017-07-07 17:30:00','Gradignan');
+UNLOCK TABLES;
