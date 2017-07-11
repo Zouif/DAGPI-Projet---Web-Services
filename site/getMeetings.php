a0062eafe7f9f20c6edc2893f75b3f92e9d25154
Samuel Bonnan <sbonnan@IUT.BX1>
2017-07-11 09:16:41 +0200

    Modif set character utf8

--- site/getMeetings.php
+++ site/getMeetings.php
@@ -3,9 +3,11 @@ require_once('init.php');
 
 $_SESSION['companyId'];
 
-$query = 'SELECT * FROM company_meeting WHERE id_company = :id_company';
+$query = 'SELECT * FROM company_meeting';
 $statement = $pdo->prepare($query, array(PDO::ATTR_CURSOR => PDO::CURSOR_FWDONLY));
-$result = $statement->execute(array(':id_company' => $_SESSION['companyId']));
+$pdo->query("SET CHARACTER SET utf8");
+$result = $statement->execute();
 
+//echo json_encode($statement->fetchAll(PDO::FETCH_ASSOC));
 echo json_encode($statement->fetchAll(PDO::FETCH_ASSOC), JSON_PRETTY_PRINT);
 ?>
