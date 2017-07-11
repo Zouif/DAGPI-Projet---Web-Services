7326e30aa519e52b22a850de117136444740c8e6
Samuel Bonnan <sbonnan@IUT.BX1>
2017-07-11 09:53:55 +0200

    Ajout de multiple br #beauté

--- site/index.php
+++ site/index.php
@@ -50,8 +50,12 @@ if( isset($_POST['login']) && isset($_POST['password']) ) {
 					<form action="" method="get" name="direction" id="direction">
 						<label>Point de départ :</label>
 						<input type="text" name="origin" id="origin">
+						<br>
+						<br>
 						<label>Destination :</label>
 						<input type="text" name="destination" id="destination">
+						<br>
+						<br>
 						<input type="button" value="Calculer l'itinéraire" onclick="javascript:calculate()">
 					</form>
 				</div>
